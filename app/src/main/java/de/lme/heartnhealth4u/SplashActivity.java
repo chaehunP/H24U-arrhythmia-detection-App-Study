3516d2679c52383ba5f6c6c37acf5e8625b4675c
chaehunP <chaehun.dev@gmail.com>
2022-06-10 14:44:01 +0900

    add SplashActivity

---
+++ app/src/main/java/de/lme/heartnhealth4u/SplashActivity.java
@@ -0,0 +1,25 @@
+package de.lme.heartnhealth4u;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try
+        {
+            Thread.sleep(2000);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
+        startActivity(new Intent(this, HeartyActivity.class));
+        finish();
+    }
+}
